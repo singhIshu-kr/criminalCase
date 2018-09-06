6336d3ec364c283c9b0c57bd042c33142ac67623
Ishu Singh <ishusi@In-Ishusi.local>
2018-09-07 01:35:24 +0530

    Remove the feedback feature

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
-const updateGuestPage = require('./storeFeedBack.js').updateGuestPage;
-const storeFeedBack = require('./storeFeedBack.js').storeFeedBack;
 const PORT = 8853;
 
 const doesFileExist = function(filename) {
@@ -26,12 +23,6 @@ const getFileType = function(filename) {
   return header[fileType];
 }
 
-const dealWithPost = function(request) {
-  request.on("data", function(text) {
-    storeFeedBack(text.toString());
-  });
-}
-
 const setHomePage = function(response) {
   response.statusCode = 302;
   response.setHeader('Location', "/index.html");
@@ -39,7 +30,7 @@ const setHomePage = function(response) {
 }
 
 const isRandomFile=function(url) {
-  let urls=["/","/feedback","/reviewGame.html"];
+  let urls=["/"];
   return !urls.includes(url);
 }
 
@@ -55,15 +46,6 @@ const checkUrlAndDisplayContent=function(request,response,fileName) {
     "/":function() {
       setHomePage(response);
       return;
-    },
-    "/feedback":function() {
-      dealWithPost(request);
-      response.statusCode = 302;
-      response.setHeader('Location', "/reviewGame.html");
-    },
-    "/reviewGame.html":function() {
-      let displayContents = updateGuestPage();
-      response.write(displayContents);
     }
   }
   handleURls(response,fileName,actionOfUrl);
